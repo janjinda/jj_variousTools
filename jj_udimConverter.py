d308ba44304fad7c138a98e956e23182d963c794
janjinda <janjinda@janjinda.com>
2019-03-23 18:43:07 +0000

    pep8 clean up

--- jj_udimConverter.py
+++ jj_udimConverter.py
@@ -4,10 +4,10 @@ from _uU_vV sequence format (e.g. from Mudbox) to a more common UDIM format (e.g
 
 Installation
 ============
-Copy the script to your own preffered location.
+Copy the script to your own preferred location.
 
 
-Windows - You have to install Python 2.7.x, if you don't have it on your system already (https://www.python.org/downloads)
+Windows - You have to have Python 2.7.x installed on your system (https://www.python.org/downloads)
 
 macOS - No need of Python installation
 
@@ -37,56 +37,57 @@ __website__ = "http://janjinda.com"
 import os
 import re
 
-def convertUDIM(type=None):
+
+def convertUDIM(cType=None):
     """Main function converts UDIM to _uU_vV and vice versa
     Parameters:
-        type (string): type of conversion
+        cType (string): type of conversion
        
     Returns:
         filesNew (string): name of renamed file
     """
+    filesNew = []
+    separator = ' ---> '
 
     for fileOld in directory:
         # Creates variables for suffix and filename
         suffix = fileOld.split(".")[-1]
-        filename = fileOld.strip('.%s' % suffix)
-        filesNew = []
-        separator = ' ---> '
+        itemName = fileOld.strip('.%s' % suffix)
 
-        if type == 'from':
+        if cType == 'from':
             # Conversion from UDIM format (e.g. 1001)
             # Finds UDIM value in last 4 characters at the end of filename
-            udim = filename[-4:]
+            udim = itemName[-4:]
 
             # Checks if udim variable is a valid UDIM format
             r = re.compile('\d{4}')
-            if r.match(udim) is None or int(udim) == 1000:
-                print ("%s%sSKIPPED" % fileOld, separator)
+            if r.match(udim) is None or int(udim) <= 1000:
+                print ("%s%sSKIPPED" % (fileOld, separator))
                 continue
-                        
+
             else:
                 # Generates u and v values from udim
                 u = int(udim[-1]) if int(udim[-1]) != 0 else 10
                 v = (int(udim[0:3]) + 1 - 100) if u != 10 else (int(udim[0:3]) - 100)
 
                 # Creates a new filename using previous variables and prints rename results
-                fileNew = ("%s_u%s_v%s.%s" % (filename.strip(udim)[:-1], u, v, suffix))
-            
-        elif type == 'to':
+                fileNew = ("%s_u%s_v%s.%s" % (itemName.strip(udim)[:-1], u, v, suffix))
+
+        elif cType == 'to':
             # Conversion from _uU_vV format (e.g. _u1_v1)
             # Checks if the _uU_vV at the end of filename is valid
-            r = re.compile('.+\_u\d+_v\d+$')
-            if r.match(filename) is None:
-                print ("%s%sSKIPPED" % fileOld, separator)
+            r = re.compile('.+_u\d+_v\d+$')
+            if r.match(itemName) is None:
+                print ("%s%sSKIPPED" % (fileOld, separator))
                 continue
 
             else:
                 # Finds u and v values in last two list items     
-                u = int((filename.split("_")[-2])[1:])
-                v = int((filename.split("_")[-1])[1:])
+                u = int((itemName.split("_")[-2])[1:])
+                v = int((itemName.split("_")[-1])[1:])
                 # Checks if u,v values are valid
-                if u == 0 or u > 10 or v==0:
-                    print ("%s%sSKIPPED" % fileOld, separator)
+                if u == 0 or u > 10 or v == 0:
+                    print ("%s%sSKIPPED" % (fileOld, separator))
                     continue
 
                 else:
@@ -95,21 +96,40 @@ def convertUDIM(type=None):
                     udimV = ('%02d' % (v - 1 + 100)) if udimU != 0 else (v + 100)
 
                     # Creates a new filename using given variables
-                    fileNew = ("%s.%s%s.%s" % ((filename.strip("_u%s_v%s" % (u, v))), udimV, udimU, suffix))
-            
+                    fileNew = ("%s.%s%s.%s" % ((itemName.strip("_u%s_v%s" % (u, v))), udimV, udimU, suffix))
+
         else:
             raise RuntimeError("Wrong conversion type given!")
-        
+
         # Prints rename results
         print ("%s ---> %s" % (fileOld, fileNew))
 
         # Finally renames files
         os.rename(path + fileOld, path + fileNew)
-        
+
         # Appends new filename to a list
         filesNew.append(fileNew)
 
     return filesNew
+
+
+while True:
+    # Asks user to input a path to desired directory
+    path = raw_input("What directory do you want to convert? ")
+
+    # If path is dragged and dropped it has '', this command removes them
+    path = path if not path.startswith("'") else path[1:-2]
+
+    # If user does not include slash at the end, this command adds it
+    path = path + "/" if not path.endswith("/") else path + ""
+
+    # If path does not exist ask again
+    if not os.path.exists(path):
+        print ("Path does not exist! Try again.")
+        continue
+    else:
+        break
+
 
 while True:
     # Asks user about conversion type and passes it to convertUV method
@@ -120,26 +140,10 @@ while True:
         continue
     else:
         break
-while True:
-    # Asks user to input a path to desired directory
-    path = raw_input("What directory do you want to convert? ")
 
-    # If path is dragged and dropped it has '', this command removes them
-    path = path if not path.startswith("'") else path[1:-2]
 
-    # If user does not include slash at the end, this command adds it
-    path = path + "/" if not path.endswith("/") else path + ""
-    
-    # If path does not exist ask again
-    if not os.path.exists(path):
-        print ("Path does not exist! Try again.")
-        continue
-    else:
-        break
-
-        
 # Lists files in directory and removes all hidden files
-directory = sorted((file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))))
-directory = [filename for filename in directory if not filename.startswith(".")]
-      
-convertUDIM(type=user)
+directory = sorted((i for i in os.listdir(path) if os.path.isfile(os.path.join(path, i))))
+directory = [ii for ii in directory if not ii.startswith(".")]
+
+convertUDIM(cType=user)
